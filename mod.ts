92dfcf7966a488a5d2c954eb629422c1359f19b2
Jenny Wakeman <tobskep@aol.com>
2024-12-15 20:47:15 -0600

    JSR-ify (part 2)

--- mod.ts
+++ mod.ts
@@ -2,12 +2,12 @@ import mustache from './mustache.mjs';
 
 const decoder = new TextDecoder();
 
-export async function renderFile(path: string, model: Object | undefined) {
+export async function renderFile(path: string, model: Object | undefined): Promise<string> {
     // @ts-ignore
     return mustache.render(decoder.decode(await Deno.readFile(path)), model);
 }
 
-export function render(body: string, model: Object | undefined) {
+export function render(body: string, model: Object | undefined): string {
     // @ts-ignore
     return mustache.render(body, model);
 }
