3363c0bd028fb4e52ba2f96e3cfadb21c996ac88
Jenny Wakeman <tobskep@aol.com>
2024-12-15 20:44:07 -0600

    JSR-ify

--- tests/basic.test.ts
+++ tests/basic.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'https://deno.land/std@0.85.0/testing/asserts.ts';
+import { assertEquals } from '@std/testing/asserts';
 import { renderFile } from '../mod.ts';
 const { test } = Deno;
 
